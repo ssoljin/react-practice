4a812edd4d1cb24a62a16d13c5b019c6fee5270e
ssoljin <soljin8122@naver.com>
2022-01-26 22:49:08 +0900

    setState의 동기 비동기성

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-  function Square(props) {
-      return (
-        <button className="square" onClick={()=>props.onClick()}>
-          {props.value}
+function Square(props) {
+    return (
+        <button className="square" onClick={() => props.onClick()}>
+            {props.value}
         </button>
-      );
-  }
+    );
+}
 
-  
-  class Board extends React.Component {
 
-    constructor(props){
+class Board extends React.Component {
+
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -28,111 +28,117 @@ import './index.css';
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X':'O';
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState(
             {
                 squares: squares,
                 xIsNext: !this.state.xIsNext
             });
-      }
+    }
 
     renderSquare(i) {
-      return (
-        <Square 
-          value={this.state.squares[i]} 
-          onClick={()=>this.handleClick(i)}
-        />
-      );
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
-  
-    render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if(winner){
-          status = 'Winner is ' + winner;
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X':'O');
-      }
 
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner is ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
-  function calculateWinner(squares) {
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+function calculateWinner(squares) {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
     return null;
-  }
+}
 
-  function Counter(props) {
-      const [count, setCount] = useState(props.initialValue);
-      return (<div>
-      Count: {count}
-      <button onClick={()=>setCount(props.initialValue)}>Reset</button>
-      <button onClick={()=>setCount(prevCount=>prevCount-1)}>-</button>
-      <button onClick={()=>setCount(prevCount=>prevCount+1)}>+</button>
-      </div>)
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-      <div>
+function Counter(props) {
+    const [count, setCount] = useState(props.initialValue);
+    return (<div>
+        Count: {count}
+        <button onClick={() => setCount(props.initialValue)}>Reset</button>
+        <button onClick={() => {
+            setCount(count - 1);
+            setCount(count - 1);
+        }}>-</button>
+        <button onClick={() => {
+            setCount(prevCount => prevCount + 1);
+            setCount(prevCount => prevCount + 1);
+        }}>+</button>
+    </div>)
+}
+
+// ========================================
+
+
+ReactDOM.render(
+    <div>
         <Game />
-        <Counter initialValue={10}/>
-      </div>,
-    
+        <Counter initialValue={10} />
+    </div>,
+
     document.getElementById('root')
-  );
-  
-  
+);
+
